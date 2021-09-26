bfb32d2d033c7d4becb40de10a4b0a68de534269
sabikunnaharsafa11 <sabiunnaharsafa92@gmail.com>
2021-09-26 12:00:58 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+             <div className="container-fluid text-center text-white bg-dark pt-4 pb-4">
+                         <h3>Programmer's Haunt</h3>
+                        <h5 className="text-danger">Budget: 100Million USD</h5>
+             </div> 
+        </div>
+    );
+};
+
+export default Header;
